c84c88b49fa8e81025530b7a53149c8a57a1d090
Christian H <haep@macbook-pro.speedport.ip>
2022-11-04 01:22:12 +0100

    changed line

--- src/DiscordFunAndGiveawayBot/DiscordFunAndGiveawayBot/Program.cs
+++ src/DiscordFunAndGiveawayBot/DiscordFunAndGiveawayBot/Program.cs
@@ -18,7 +18,6 @@ namespace DiscordFunAndGiveawayBot
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
