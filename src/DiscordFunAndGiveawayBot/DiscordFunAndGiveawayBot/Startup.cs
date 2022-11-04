1c224ffc8a2e15152b1b76a51cf61334f5f11aff
Christian Häp <christian@haep.net>
2022-11-04 08:56:24 +0100

    Removed warnings in projects

--- src/DiscordFunAndGiveawayBot/DiscordFunAndGiveawayBot/Startup.cs
+++ src/DiscordFunAndGiveawayBot/DiscordFunAndGiveawayBot/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace DiscordFunAndGiveawayBot
@@ -28,7 +29,7 @@ namespace DiscordFunAndGiveawayBot
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IDiscordClient discordClient, ICultureHelper cultureHelper)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDiscordClient discordClient, ICultureHelper cultureHelper)
         {
             if (env.IsDevelopment())
             {
